299d7f8d839fd16b3f7be90f9d8b2c7f1e3af926
uwakrini <15220593@bsi.ac.id>
2024-03-25 22:41:20 +0700

    tugas1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,8 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Rini Afriani
-        Saya sekaranag tinggal di serdam
-        olahraga yang saya sukai adalah Bulutangkis";
+        echo"Nama saya Rini Afriani, 
+        Saya sekaranag tinggal di serdam gg Ceria VII no B5 <br>
+        Saya adalah mahasiswa UBSI semester 4<br>
+        Saya adalah orang yang sangat suka dengan kucing";
     }
 }
