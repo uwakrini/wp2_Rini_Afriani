f365fefa08cc2ce95141bd2a2b85ee6b5daec09a
uwakrini <15220593@bsi.ac.id>
2024-03-25 20:39:16 +0700

    Tugas1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    ini adalah contoh1
-</body>
-</html>
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rini Afriani
+        Saya sekaranag tinggal di serdam
+        olahraga yang saya sukai adalah BulutangkisS
+    }
+}
